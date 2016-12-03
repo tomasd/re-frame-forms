e48e17ba89a65531f6209a10c44b5d9dd34bf4d8
Tomas Drencak <tomas@drencak.com>
2016-12-03 14:22:36 +0100

    Version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-frame-forms "0.1.3"
+(defproject re-frame-forms "0.1.4-SNAPSHOT"
   :description "Reagent library for form processing"
   :url "https://github.com/tomasd/re-frame-forms"
   :license {:name "MIT"}
